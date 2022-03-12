a3edf54a39029ffc1cf49170d25bccb6ba6bf9ad
Bloodcryme <diogogomes95@hotmail.com>
2022-03-12 01:20:34 +0000

    cat api maybe fix

--- Commands/cat.js
+++ Commands/cat.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'cat',
     description: 'Post a cat picture',
     execute(message, args) {
-        const { image } = fetch(' http:\/\/aws.random.cat\/meow')
+        const { image } = fetch('https://aws.random.cat/meow')
         .then(response => response.json())
         .then(data => message.channel.send(data.file))
     }
