47a6daa3cb6869453285af2e3c271319abb9cec0
Bloodcryme <diogogomes95@hotmail.com>
2022-04-15 15:59:07 +0100

    Test 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 const prefix = "!";
-// const config = require('./config.json');
+const config = require('./config.json');
 const fs = require('fs');
 client.commands = new Discord.Collection();
 
@@ -118,4 +118,4 @@ client.on('message', async message => {
     } 
 }})
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(config.token);
